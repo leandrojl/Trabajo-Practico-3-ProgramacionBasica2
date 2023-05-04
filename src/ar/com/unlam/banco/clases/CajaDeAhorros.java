ec30494cb7c77af68fc4c5117e81961e7cab3552
leandrojl <leandroracing96@hotmail.com>
2023-05-03 22:33:54 -0300

    Test cajadeahorros + interes

--- src/ar/com/unlam/banco/clases/CajaDeAhorros.java
+++ src/ar/com/unlam/banco/clases/CajaDeAhorros.java
@@ -1,8 +1,27 @@
 package ar.com.unlam.banco.clases;
 
 public class CajaDeAhorros extends Cuenta{
+	
+	Integer numeroDeExtracciones;
 
 	public CajaDeAhorros() {
 		super();
+		numeroDeExtracciones=0;
 	}
+	
+	@Override
+	public Double extraer(Double monto) {
+		
+		numeroDeExtracciones=numeroDeExtracciones+1;
+		
+		if(numeroDeExtracciones <6){
+			
+			this.saldo= saldo-monto;
+			return saldo;
+		}else {
+			this.saldo= (saldo-monto)-6;
+			return saldo;
+		}
+		
+	};
 }
--- test/ar/com/unlam/test/banco/TestBanco.java
+++ test/ar/com/unlam/test/banco/TestBanco.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import ar.com.unlam.banco.clases.Banco;
+import ar.com.unlam.banco.clases.CajaDeAhorros;
 import ar.com.unlam.banco.clases.Cliente;
 import ar.com.unlam.banco.clases.Cuenta;
 import ar.com.unlam.banco.clases.CuentaSueldo;
@@ -52,6 +53,25 @@ public class TestBanco {
 		cuentasueldo.extraer(2000.0);
 		
 		Assert.assertTrue(cuentasueldo.getSaldo().equals(montoADepositar));
+	}
+	
+	@Test
+	public void cuandoUnaCajaDeAhorroHaceLaSextaExtraccionLeCobreUnExtraDe6() {
+		CajaDeAhorros cajaDeAhorro = new CajaDeAhorros();
+		
+		Double montoADepositar= 10000.0;
+		Double montoEsperado= 3994.0;
+		
+		cajaDeAhorro.depositar(montoADepositar);
+		cajaDeAhorro.extraer(1000.0);
+		cajaDeAhorro.extraer(1000.0);
+		cajaDeAhorro.extraer(1000.0);
+		cajaDeAhorro.extraer(1000.0);
+		cajaDeAhorro.extraer(1000.0);
+		cajaDeAhorro.extraer(1000.0);
+		
+		Assert.assertEquals(montoEsperado, cajaDeAhorro.getSaldo());
+		
 	}
 	
 	public void queSeCobreCincoPorcientoDeComisionAlDepositarDineroLuegoDeHaberRealizadoUnaExtraccionMayorAlSaldo() {
